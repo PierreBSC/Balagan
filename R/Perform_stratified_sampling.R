cfdd76a73cc358b4ff29c4efb4e8ba3f38ae1c43
PierreBSC <pierre.bost@gmail.com>
2023-11-06 17:00:38 +0100

    Update Perform_stratified_sampling.R

--- R/Perform_stratified_sampling.R
+++ R/Perform_stratified_sampling.R
@@ -19,12 +19,6 @@
 #' @import tiff 
 #' @export
 
-smart.round <- function(x) {
-  y <- floor(x)
-  indices <- tail(order(x-y), round(sum(x)) - sum(y))
-  y[indices] <- y[indices] + 1
-  y
-}
 
 Perform_stratified_sampling_simulation = function(sce,tiff_file,panel_file,N_simulations=30,N_FoV=10,FoV_size=100,type_stratification="Neyman",
                                                   L=6,sigma=100,specific_channels=NULL,perform_rotation=TRUE,show_plot=TRUE) {
